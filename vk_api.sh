ee37db13553f5467643c7e1a14ab5f4c26d2dc3c
Albert <albert.fz@yandex.ru>
2022-08-03 11:38:10 +0500

    add curl error check

--- vk_api.sh
+++ vk_api.sh
@@ -4,23 +4,31 @@ panic() {
 	echo "$1" >&2 ; exit 1
 }
 
+warning() {
+	echo "$1" >&2
+}
+
 vk_api() (
 	set -- "$@" "access_token=${VK_TOKEN}" "v=${VERSION}"
 	method="$1"; shift
 	args=$(IFS='&'; echo "$*")
 
-	curl -s -X POST --data "$args" "https://api.vk.com/method/${method}" |
-		jq -rc ".response, .error.error_code, .error.error_msg" | (
+	if result=$(curl -sS -X POST --data "$args" "https://api.vk.com/method/${method}" 2>&1)
+	then
+		echo "$result" | 
+			jq -rc ".response, .error.error_code, .error.error_msg" | (
+				read -r response
+				read -r error_code
+				read -r error_msg
 
-			read -r response
-			read -r error_code
-			read -r error_msg
+				[ "$error_code" != null ] &&
+					panic "VK_API_ERROR[${error_code}]: ${error_msg}"
 
-			[ "$error_code" != null ] &&
-				panic "VK_API_ERROR[${error_code}]: ${error_msg}"
-
-			echo "$response"
+				echo "$response"
 		)
+	else
+		panic "CURL_ERROR[$?]: $(echo "$result" | cut -d' ' -f3)"
+	fi
 
 )
 
@@ -32,20 +40,26 @@ bots_long_poll() (
 			read -r ts
 
 			while true; do
-				answ=$(curl -s "${server}?act=a_check&key=${key}&ts=${ts}&wait=25")
-				failed=$(echo "$answ" | jq -rc '.failed')
+
+				if ! answ=$(curl -sS "${server}?act=a_check&key=${key}&ts=${ts}&wait=25" 2>&1)
+				then
+					warning "$answ"
+					sleep 1
+					continue
+				fi
 				
+				failed=$(echo "$answ" | jq -rc '.failed')
 				case "$failed" in
 					null)
 						echo "$answ" | jq -rc '.updates | .[]'
 						;;
-					'' | 2 | 3)
+					2 | 3)
 						answ=$(vk_api groups.getLongPollServer group_id="$GROUP_ID")
 						key=$(echo "$answ" | jq -rc '.key')
 						;;
 				esac
 				
-				ts=$(echo "$answ" | jq -rc ".ts // $ts")
+				ts=$(echo "$answ" | jq -rc ".ts")
 			done
 		)
 )
