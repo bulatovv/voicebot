dfad8200c6449b34f5850945d3ee20c9353bf963
Albert <albert.fz@yandex.ru>
2022-08-03 11:37:55 +0500

    add string utils

--- utils.sh
+++ utils.sh
@@ -5,11 +5,38 @@ RANDSTATE=$((RANDSTATE + $$))
 rand() {
 	RANDSTATE=$((RANDSTATE * 1103515245 + 12345))
 	( 
-		result=$(( RANDSTATE / 65536 % 32768 ))
+		result=$((RANDSTATE / 65536 % 32768))
 		echo "${result#-}"
 	)
 }
 
-pick_random() (
+pick_random() {
 	eval echo \$$(($(rand) % $# + 1))
-)
+}
+
+startswith() {
+	case "$1" in
+		"$2"*)
+			return 0
+			;;
+	esac	
+	return 1
+}
+
+endswith() {
+	case "$1" in
+		*"$2")
+			return 0
+			;;
+	esac	
+	return 1
+}
+
+contains() {
+	case "$1" in
+		*"$2"*)
+			return 0
+			;;
+	esac	
+	return 1
+}
